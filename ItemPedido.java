6c06254d0fbc01730133ace6f8d7a6dbf665ca3c
nicolasjesus9464-cloud <nicolasjesus9464@gmail.com>
2026-04-08 10:10:42 -0300

    Create ItemPedido class for order items management
    o item pedido ai qualquer coisa a gente altera

---
+++ ItemPedido.java
@@ -0,0 +1,40 @@
+public class ItemPedido {
+
+
+    private produto produto;
+    private int quantidade;
+
+
+
+    public ItemPedido(produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+
+    }
+
+public Produto getProduto() {
+    return produto;
+}
+
+
+
+public int getQuantidade() {
+    return quantidade;
+
+}
+
+
+public void setQuantidade (int quantidade) {
+    if (quantidade > 0) {
+    this.quantidade = quantidade;
+    }
+}
+
+
+
+public double getTotal()  {
+    return produto.getPreco() * quantidade;
+}
+
+
+}
