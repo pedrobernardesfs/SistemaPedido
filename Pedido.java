3dea54a1ae1debf74d6c9891721a90d2ef4f64f5
VNsdemo <viniciusicaro2077@gmail.com>
2026-04-10 22:08:51 -0300

    Add files via upload
    ta ai o carai do pedido

--- Pedido.java
+++ Pedido.java
@@ -1,4 +1,37 @@
-public class Pedido{
+import java.time.LocalDate;
 
-  
+public class Pedido {
+    private int numero;
+    private LocalDate data;
+    private Cliente cliente;
+
+    public Pedido(Cliente cliente) {
+        this.numero = (int)(Math.random() * 90000) + 10000;
+        this.data = LocalDate.now();
+        this.cliente = cliente;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
 }
