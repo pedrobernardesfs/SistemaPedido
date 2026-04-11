04c82d62c175b49b79067ece6d79ef6519cbf17b
Pedro Bernardes <pedrobernardesfs@gmail.com>
2026-04-10 22:08:44 -0300

    Update Produto.java
    Consertei cagada do vinicius kkkkk

--- Produto.java
+++ Produto.java
@@ -1,37 +1,5 @@
-import java.time.LocalDate;
+public class produto {
 
-public class Pedido {
-    private int numero;
-    private LocalDate data;
-    private Cliente cliente;
 
-    public Pedido(Cliente cliente) {
-        this.numero = (int)(Math.random() * 90000) + 10000;
-        this.data = LocalDate.now();
-        this.cliente = cliente;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public LocalDate getData() {
-        return data;
-    }
-
-    public void setData(LocalDate data) {
-        this.data = data;
-    }
-
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
+    
 }
